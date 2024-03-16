da273fdbc5908f5ef261e26a38fd32db6283cafd
mohwasthere <mohamedhussamsaba@gmail.com>
2024-03-16 14:58:42 +0200

    test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	"""reads defined words in hidden_4"""
-	import hidden_4
+    """reads defined words in hidden_4"""
+    import hidden_4
 
-	names = dir(hidden_4)
-	for name in names:
-		if name[:2] != "__":
-			print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
