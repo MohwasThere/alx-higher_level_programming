8fbd520fc20ec3d693e6cf68bdd935df541cf7ad
mohwasthere <mohamedhussamsaba@gmail.com>
2024-03-17 11:39:51 +0200

    test again

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,41 +10,40 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
-	listint_t *temp = *head;
+	listint_t *temp;
 	int flag = 0;
 
-	if (head != NULL)
+	if (newnode == NULL)
+		return (NULL);
+
+
+	temp = *head;
+	newnode->n = number;
+	newnode->next = NULL;
+
+	if (head == NULL)
 	{
-		while (!(temp->n <= number && temp->next->n >= number))
-		{
-			if (temp->n <= number && temp->next->n >= number)
-			{
-				newnode->n = number;
-				newnode->next = temp->next;
-				temp->next = newnode;
-				return (newnode);
-			}
-			else if ((temp->n >= number) && flag == 0)
-			{
-				newnode->n = number;
-				newnode->next = temp;
-				temp->next = newnode;
-				return (newnode);
-			}
-			else
-			{
-				return (NULL);
-			}
-			temp = temp->next;
-			flag++;
-		}
-	}
-	else
-	{
-		newnode->n = number;
-		temp->next = newnode;
-		newnode->next = NULL;
+		*head = newnode;
 		return (newnode);
 	}
+
+	while (temp->next != NULL)
+	{
+		if (temp->n <= number && temp->next->n >= number)
+		{
+			newnode->next = temp->next;
+			temp->next = newnode;
+			return (newnode);
+		}
+		else if ((temp->n >= number) && flag == 0)
+		{
+			newnode->next = temp;
+			temp->next = newnode;
+			return (newnode);
+		}
+		temp = temp->next;
+		flag++;
+	}
+	free_listint(temp);
 	return (NULL);
 }
