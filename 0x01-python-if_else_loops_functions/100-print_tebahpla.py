c4557527cf9387e4ea6e973dcb7e6bff303d06f8
mohwasthere <mohamedhussamsaba@gmail.com>
2024-07-29 17:30:12 +0300

    pycodestyle fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range (96+26,96,-1):
-    print('{}'.format(chr(i - 32*(i%2))), end="")
+for i in range(96+26, 96, -1):
+    print('{}'.format(chr(i - 32*(i % 2))), end="")
