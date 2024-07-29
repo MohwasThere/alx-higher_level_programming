164f75f47d834bf4e27c1989eca77cdbb7932d90
mohamed saba <mohamedhussamsaba@gmail.com>
2024-07-29 17:23:28 +0300

    IM BAACK

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 97+26):
-    if i == 101:
-        continue
-    elif i == 113:
+    if i == 101 or i == 113:
         continue
     else:
         print('{}'.format(chr(i)), end="")
